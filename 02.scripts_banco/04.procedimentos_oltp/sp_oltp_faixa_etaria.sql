f329f33d76365813940fe2febac5090d4a861702
SamuelAnjos <61122752+samuelAnjos@users.noreply.github.com>
2022-11-11 08:11:52 -0300

    Update sp_oltp_faixa_etaria.sql

--- 02.scripts_banco/04.procedimentos_oltp/sp_oltp_faixa_etaria.sql
+++ 02.scripts_banco/04.procedimentos_oltp/sp_oltp_faixa_etaria.sql
@@ -3,6 +3,6 @@ as
 begin
 
 	insert into tb_aux_faixa_etaria
-	select @data_carga, cod_faixa_etaria, idade_inicial, idade_final, faixa_etaria
+	select @dataCarga, cod_faixa_etaria, idade_inicial, idade_final, faixa_etaria
 	from tb_faixa_etaria
 end
