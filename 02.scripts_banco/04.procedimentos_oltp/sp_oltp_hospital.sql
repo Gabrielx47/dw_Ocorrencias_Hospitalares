7f0dd2d1611d1f7983c5b4b9ee0b9fceef7f1213
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-11-11 08:32:09 -0300

    Samuel, realizei uma pequena alteração no código do procedimento que você criou. Done #17

--- 02.scripts_banco/04.procedimentos_oltp/sp_oltp_hospital.sql
+++ 02.scripts_banco/04.procedimentos_oltp/sp_oltp_hospital.sql
@@ -1,9 +1,8 @@
 create or alter procedure sp_oltp_hospital(@dataCarga datetime)
 as
 begin
-
-    
 	insert into tb_aux_hospital
-	select @data_carga, cod_hospital, nome, rua, bairro, cidade
+	-- CNPJ = COD_HOSPITAL
+	select @data_carga, CNPJ, nome, rua, bairro, cidade
 	from tb_hospital
 end
