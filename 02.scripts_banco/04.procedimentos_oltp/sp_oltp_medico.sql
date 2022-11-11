73edfad6243b018c9b5c03264672fcd1a9593fcf
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-11-11 08:42:31 -0300

    Samuel, realizei uma pequena alteração tendo como objetivo torna o procedimento funcional. Done #22

--- 02.scripts_banco/04.procedimentos_oltp/sp_oltp_medico.sql
+++ 02.scripts_banco/04.procedimentos_oltp/sp_oltp_medico.sql
@@ -1,9 +1,7 @@
 create or alter procedure sp_oltp_medico(@dataCarga datetime)
 as
 begin
-
-    
 	insert into tb_aux_medico
-	select @data_carga, cod_medico, nome, crm, especialidade
+	select @dataCarga, cod_medico, nome, crm
 	from tb_medico
 end
