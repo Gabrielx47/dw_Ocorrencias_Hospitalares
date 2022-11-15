7c6d7cf70a2a7c8be7bfd1351f5bf4088f7e65d1
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-11-14 21:15:53 -0300

    O procedimento sp_oltp_ocorrencia está totalmente pronto. Done #31

--- 02.scripts_banco/04.procedimentos_oltp/sp_oltp_ocorrencia.sql
+++ 02.scripts_banco/04.procedimentos_oltp/sp_oltp_ocorrencia.sql
@@ -4,8 +4,9 @@ create or alter procedure sp_oltp_ocorrencia(@dataCarga datetime)
 as
 BEGIN
 	insert into TB_AUX_OCORRENCIA
-	select @dataCarga, O.CNPJ, p.DATA_NASCIMENTO, 
+	select @dataCarga, O.CNPJ, p.DATA_NASCIMENTO, d.COD_DOENCA, m.COD_MEDICO, p.COD_SUS, 1 
 	from TB_OCORRENCIA O inner join TB_HOSPITAL h on(O.CNPJ = h.CNPJ) 
 	inner join TB_PACIENTE p on(O.COD_SUS = p.COD_SUS) 
-	inner join TB_DOENCA d on(O.COD_OCORRENCIA)
+	inner join TB_DOENCA d on(O.COD_DOENCA = d.COD_DOENCA)
+	inner join TB_MEDICO m on(O.COD_MEDICO = m.COD_MEDICO)
 END
