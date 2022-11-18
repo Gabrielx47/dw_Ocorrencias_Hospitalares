c801ede5864503a994d9f710292b2ac87f037788
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2022-11-17 22:40:06 -0300

    Uma simples alteração foi realizada no procedimento sp_dim_doenca. Done #37

--- 02.scripts_banco/05.procedimentos_dw/sp_dim_doenca.sql
+++ 02.scripts_banco/05.procedimentos_dw/sp_dim_doenca.sql
@@ -3,7 +3,7 @@ use dw_ocorrencias_hospitalares
 create or alter procedure sp_dim_doenca
 as
 BEGIN
-	insert into DIM_MEDICO
+	insert into DIM_DOENCA
 	select COD_DOENCA, NOME
-	from TB_AUX_MEDICO
+	from TB_AUX_DOENCA
 END
